78ce0d673fddfa35a3a4438ea4c358eba941dfc8
Amadeus Wieczorek <git@amadeusw.com>
2014-11-14 20:34:46 -0500

    Adds a bundle with tanks-related js
    Also, sets BundleTable.EnableOptimizations = false;

--- src/Tanks/App_Start/BundleConfig.cs
+++ src/Tanks/App_Start/BundleConfig.cs
@@ -23,13 +23,16 @@ namespace Tanks
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/tanks").Include(
+                      "~/Scripts/pixi.dev.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
